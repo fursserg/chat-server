495a28dd3dd4bfd3156003ebafc0e64821f7f421
Sergei Fursov <sergei.fursov@softgamings.com>
2024-07-16 19:29:20 +0300

    fix homework1

--- cmd/main.go
+++ cmd/main.go
@@ -3,42 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
-	"google.golang.org/protobuf/types/known/emptypb"
 	"log"
 	"net"
 
 	"github.com/brianvoe/gofakeit"
-	chat "github.com/fursserg/chat-server/pkg/chat_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	
+	chat "github.com/fursserg/chat-server/pkg/chat_v1"
 )
 
 const grpcPort = 50052
 
 type server struct {
 	chat.UnimplementedChatV1Server
-}
-
-func (s *server) Create(ctx context.Context, req *chat.CreateRequest) (*chat.CreateResponse, error) {
-	log.Printf("Chat create data: %+v", req.GetUsernames())
-
-	return &chat.CreateResponse{
-		Id: gofakeit.Int64(),
-	}, nil
-}
-
-func (s *server) Delete(ctx context.Context, req *chat.DeleteRequest) (*emptypb.Empty, error) {
-	log.Printf("Chat delete id: %d", req.GetId())
-
-	res := new(emptypb.Empty)
-	return res, nil
-}
-
-func (s *server) SendMessage(ctx context.Context, req *chat.SendMessageRequest) (*emptypb.Empty, error) {
-	log.Printf("Send message data: %+v", req)
-
-	res := new(emptypb.Empty)
-	return res, nil
 }
 
 func main() {
@@ -56,4 +35,25 @@ func main() {
 	if err = s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+}
+
+func (s *server) Create(ctx context.Context, req *chat.CreateRequest) (*chat.CreateResponse, error) {
+	log.Printf("Chat create users ids: %+v", req.GetUserIds())
+	log.Printf("Chat create name: %s", req.GetName())
+
+	return &chat.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (s *server) Delete(ctx context.Context, req *chat.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Chat delete id: %d", req.GetId())
+
+	return new(emptypb.Empty), nil
+}
+
+func (s *server) SendMessage(ctx context.Context, req *chat.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("Send message data: %+v", req)
+
+	return new(emptypb.Empty), nil
 }
